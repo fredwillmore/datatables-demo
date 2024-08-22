3829d9d1cd87ca6c3b2eb5d19630e25ac193756c
Fred Willmore <fredwillmore@gmail.com>
2024-08-21 19:28:57 -0700

    set draw value for view rendering

--- app/controllers/asteroids_controller.rb
+++ app/controllers/asteroids_controller.rb
@@ -5,12 +5,16 @@ class AsteroidsController < ApplicationController
   # GET /asteroids.json
   def index
     respond_to do |format|
+      @draw = params[:draw] || 1
       @asteroids = Asteroid.search(
         params.dig(:search, :value),
-        { page: page,
+        {
+          page: page,
           page_length: page_length,
           sort_order: sort_order,
-          sort_direction: sort_direction } )
+          sort_direction: sort_direction
+        }
+      )
       format.html
       format.json
     end
--- app/controllers/comets_controller.rb
+++ app/controllers/comets_controller.rb
@@ -2,6 +2,7 @@ class CometsController < ApplicationController
 
   def index
     respond_to do |format|
+      @draw = params[:draw] || 1
       @comets = Comet.search(
         params.dig(:search, :value),
         { page: page,
@@ -9,9 +10,7 @@ class CometsController < ApplicationController
           sort_order: sort_order,
           sort_direction: sort_direction } )
       format.html
-      format.json do
-        render_datatable_json @comets
-      end
+      format.json
     end
   end
 

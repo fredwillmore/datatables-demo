5e2344fa9391764389a3cb9c975e8bab139abfed
Fred Willmore <fredwillmore@gmail.com>
2024-08-21 19:58:22 -0700

    removed views on deprecated routes

--- spec/routing/asteroids_routing_spec.rb
+++ spec/routing/asteroids_routing_spec.rb
@@ -6,34 +6,5 @@ RSpec.describe AsteroidsController, type: :routing do
     it "routes to #index" do
       expect(:get => "/asteroids").to route_to("asteroids#index")
     end
-
-    it "routes to #new" do
-      expect(:get => "/asteroids/new").to route_to("asteroids#new")
-    end
-
-    it "routes to #show" do
-      expect(:get => "/asteroids/1").to route_to("asteroids#show", :id => "1")
-    end
-
-    it "routes to #edit" do
-      expect(:get => "/asteroids/1/edit").to route_to("asteroids#edit", :id => "1")
-    end
-
-    it "routes to #create" do
-      expect(:post => "/asteroids").to route_to("asteroids#create")
-    end
-
-    it "routes to #update via PUT" do
-      expect(:put => "/asteroids/1").to route_to("asteroids#update", :id => "1")
-    end
-
-    it "routes to #update via PATCH" do
-      expect(:patch => "/asteroids/1").to route_to("asteroids#update", :id => "1")
-    end
-
-    it "routes to #destroy" do
-      expect(:delete => "/asteroids/1").to route_to("asteroids#destroy", :id => "1")
-    end
-
   end
 end

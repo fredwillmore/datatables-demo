6956d1bb0cd98bf71326f3f666ac3a078d466725
Fred Willmore <fredwillmore@gmail.com>
2024-08-22 17:35:34 -0700

    stub results from datatable_columns so it's more configurable

--- spec/views/comets/index.html.erb_spec.rb
+++ spec/views/comets/index.html.erb_spec.rb
@@ -1,11 +1,18 @@
 require 'rails_helper'
 
 describe "comets/index", type: :view do
-  it "renders the table headers with text" do
+  before do
+    allow(Comet).to receive(:datatable_columns).and_return(
+      {
+        id: { header_text: "ID Foo", orderable: 'false', searchable: 'false' },
+        full_name: { header_text: "Full Name Config Value", searchable: 'true', orderable: 'true' },
+      }
+    )
+  end
+
+  it "renders the table headers with text provided by datatable_columns" do
     render
-    expect(rendered).to have_text("ID")
-      .and have_text("Full Name")
-      .and have_text("Diameter (km)")
-      .and have_text("Albedo")
+    expect(rendered).to have_text("ID Foo")
+      .and have_text("Full Name Config Value")
   end
 end
